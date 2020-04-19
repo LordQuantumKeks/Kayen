d03b81195bfba7d9e3796a3d6aa9b49d87190e94
LordQuantumKeks <lordquantumkeks@gmail.com>
2020-04-20 00:38:58 +0200

    Added Settingsfile Edit + Bugfixes

--- Python Verarbeitung/einstellungen_weiterverarbeitung.py
+++ Python Verarbeitung/einstellungen_weiterverarbeitung.py
@@ -1,17 +1,30 @@
 import re
 import os
 
-
 with open('../webseite/changes.txt', 'r') as currentfile:
     change = currentfile.read()
     change = re.sub('[{}" ]', '', change)
-    print(change)
-
-print('writing ' + change + ' to general_settings.txt\n')
+    changeName, seperator, changeValue = change.rpartition(":")
 
 with open('../PipeData/general_settings.txt', 'r+') as currentfile:
     data = currentfile.readlines()
+    x = -1
     for line in data:
-        print(line)
+        x += 1
+        if changeName in line:
+            print(changeName, ' ', changeValue)
+            replacementline = changeName + "=" + changeValue + "\n"
+            data[x] = replacementline
+    currentfile.seek(0)
+    currentfile.truncate(0)
+    currentfile.writelines(data)
 
 
+
+
+
+
+# luft_rpm=280
+# bodenfeuchteschwelle=12
+# licht_takt=43200
+# temp=22.5
