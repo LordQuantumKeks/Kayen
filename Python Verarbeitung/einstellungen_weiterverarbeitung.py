574d13ee99e60dce9ac95e926de4f7c174b20b86
LordQuantumKeks <lordquantumkeks@gmail.com>
2020-04-23 12:38:31 +0200

    Restructured Data and polished out some python files

--- Python Verarbeitung/einstellungen_weiterverarbeitung.py
+++ Python Verarbeitung/einstellungen_weiterverarbeitung.py
@@ -7,13 +7,13 @@ while True:
         change = re.sub('[{}" ]', '', change)
         changeName, seperator, changeValue = change.rpartition(":")
 
-    with open('../PipeData/general_settings.txt', 'r+') as recentfile:
+    with open('../Webseite/static/general_settings.txt', 'r+') as recentfile:
         data = recentfile.readlines()
         x = -1
         for line in data:
             x += 1
             if changeName in line:
-                print(changeName, ' ', changeValue)
+                # print(changeName, ' ', changeValue) ########## THIS IS FOR DEBUGGING PURPOSES ONLY ###############
                 replacementline = changeName + "=" + changeValue + "\n"
                 data[x] = replacementline
         recentfile.seek(0)
@@ -21,6 +21,8 @@ while True:
         recentfile.writelines(data)
 
 
+
+###### SAMPLE DATA ######
 # luft_rpm=280
 # bodenfeuchteschwelle=12
 # licht_takt=43200
